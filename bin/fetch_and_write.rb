ef1228cba5aea9946a8dc322513b3d988f0e895d
Michael Currin <michael.currin@gmail.com>
2020-01-25 15:11:08 +0200

    refactor: Update script

--- bin/fetch_and_write.rb
+++ bin/fetch_and_write.rb
@@ -11,14 +11,20 @@ PAY_LOAD = {
   "Foo": "bar",
   "Fuzz": 123
 }
-resp = Faraday.post(
-  'http://httpbin.org/post',
-  PAY_LOAD.to_json,
-  "Content-Type" => "application/json"
-)
-resp_data = JSON.parse resp.body
-resp_json = resp_data['json']
-pretty_json_str = JSON.pretty_generate(resp_json)
+API_URL = 'http://httpbin.org/post'
+
+def do_post(url, payload)
+  resp = Faraday.post(
+    url,
+    payload.to_json,
+    "Content-Type" => "application/json"
+  )
+  puts "Status: #{resp.status}"
+
+  resp_data = JSON.parse resp.body
+  resp_data['json']
+end
+
 
 public_dir = '_site'
 Dir.mkdir(public_dir) unless File.exists?(public_dir)
@@ -26,4 +32,8 @@ Dir.mkdir(public_dir) unless File.exists?(public_dir)
 new_filename = 'api_data.json'
 new_filepath = "#{public_dir}/#{new_filename}"
 
+json_data = do_post(API_URL, PAY_LOAD)
+
+puts "Write file: #{new_filepath}"
+pretty_json_str = JSON.pretty_generate(json_data)
 File.open(new_filepath, 'w') { |file| file.write(pretty_json_str) }
